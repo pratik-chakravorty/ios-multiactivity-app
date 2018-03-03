a61bf879a89457bd84e4419dbfd9b1ac12a274d0
user136776 <user136776@eu234.local>
2018-03-03 18:28:58 +0000

    first commit

---
+++ XMLMovieParser.swift
@@ -0,0 +1,13 @@
+//
+//  XMLMovieParser.swift
+//  App1
+//
+//  Created by user136776 on 3/1/18.
+//  Copyright © 2018 user136776. All rights reserved.
+//
+
+import UIKit
+
+class XMLMovieParser: NSObject {
+
+}
